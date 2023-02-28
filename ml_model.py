86e9c93cd501e3e8d379bcec320ecc457d5ce5f0
iamnarendrasingh <narendraa.singhshekhawat@gmail.com>
2023-02-28 23:27:19 +0530

    Adding model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,14 @@
 #All ML files here
+
+print("Adding all ML related code here")
+
+def model1():
+	print("Adding code for model1")
+
+
+def model2():
+	print("Adding code for model2")
+
+
+def model3():
+	print("Adding code for model3")
